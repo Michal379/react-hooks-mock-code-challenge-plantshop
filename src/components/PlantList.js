bf65241075ff35abd5c2274491c59ef5fac08d9c
Michal <khakaimic4@gmail.com>
2023-06-15 02:37:16 +0300

    first commit

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PlantCard from "./PlantCard";
 
 function PlantList() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((response) => response.json())
+      .then((responseData) => setData(responseData))
+      .catch((error) => console.log(error));
+  }, []);
+
+  console.log(data);
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {data.map((plant) => (
+        <PlantCard key={plant.id} plant={plant} />
+      ))}
+    </ul>
   );
 }
 
